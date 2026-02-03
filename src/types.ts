12afc33d8f22cb67728907210780bdd5a4a521bc
DIEHARDERS <cybrobo@gmail.com>
2026-02-02 17:05:36 -0800

    rename search types

--- src/types.ts
+++ src/types.ts
@@ -766,7 +766,7 @@ export interface I_VectorSearchRequest {
   collections?: string[]
   top_k?: number
   max_preview?: number
-  max_extract?: number
+  max_read?: number
   auto_expand?: boolean
 }
 
@@ -776,7 +776,7 @@ export interface I_WebSearchRequest {
   website?: string
   max_pages?: number
   max_preview?: number
-  max_extract?: number
+  max_read?: number
 }
 
 // File System Search
@@ -784,8 +784,8 @@ export interface I_FileSystemSearchRequest {
   query: string
   directories: string[]
   file_patterns?: string[]
-  max_files_preview?: number
-  max_files_parse?: number
+  max_preview?: number
+  max_read?: number
   max_iterations?: number
   auto_expand?: boolean
 }
@@ -800,7 +800,7 @@ export interface I_StructuredSearchRequest {
   }>
   group_by?: string
   max_preview?: number
-  max_extract?: number
+  max_read?: number
   auto_expand?: boolean
 }
 
